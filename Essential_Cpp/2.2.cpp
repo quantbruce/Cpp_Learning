8b36666b75e509f5eeea795d72f03682010ff29c
quantbruce <48423247+quantbruce@users.noreply.github.com>
2021-05-15 21:05:18 +0800

    Create 2.2.cpp

---
+++ Essential_Cpp/2.2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+bool calc_elements(vector<int> &vec, int pos);
+void display(vector<int> &vec, const string &title, ostream &os=cout);
+
+
+int main()
+{
+    vector<int> pent;
+    const string title("Pentagonal Numeric Series" );
+    if(calc_elements(pent, 0))
+        display(pent, title);
+    if(calc_elements(pent, 8))
+        display(pent, title);
+    if(calc_elements(pent, 14))
+        display(pent, title);
+    if(calc_elements(pent, 138))
+        display(pent, title);
+    return 0;
+}
+
+bool calc_elements(vector<int> &vec, int pos)
+{
+    if(pos <= 0 || pos > 64)
+    {
+        cout << "Sorry, invaild pos " << pos << endl;
+        return false;
+    }
+
+    for(int ix=vec.size()+1; ix<=pos; ++ix)
+    {
+        vec.push_back(ix*(3*ix-1)/2);
+    }
+    return true;
+}
+
+void display(vector<int> &vec, const string &title, ostream &os)
+{
+    cout << '\n' << title << "\n\t";
+    for(int ix=0; ix < vec.size(); ++ix)
+        os << vec[ix] << ' ';
+    os << endl;
+}
